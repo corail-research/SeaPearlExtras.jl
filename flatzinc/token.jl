d7b2a94cb0606c79f72f7fe8ce1859147162c01b
Kim Rioux-Paradis <kim.rioux-paradis.1@ulaval.ca>
2021-07-23 11:04:22 -0400

    add first token type

---
+++ flatzinc/token.jl
@@ -0,0 +1,17 @@
+@enum TokenType begin
+    VAR = 1
+    INT = 2
+    COLON = 3
+    EQUAL = 4
+end
+
+struct Token
+    type::TokenType
+    value
+end
+
+
+function Base.show(io::IO, token::Token) 
+    print("Type : " * string(token.type) * "\nValue : " * string(token.value))
+end
+
