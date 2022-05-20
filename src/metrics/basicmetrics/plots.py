70bd25bcb9acaed0e3badbc47e69d514f15bbd0c
Tristan François <33726628+gostreap@users.noreply.github.com>
2022-05-20 09:47:14 -0400

    Saves figures and adds a facility function `all` in `plots.py`  (#12)
    * Add save_fig + all function to plots.py
    
    * Fix figure size problem
    
    * Pass parameter to summary in all
    
    * Add '_' to regex for training
    
    * Add __pycache__ to gitignore

--- src/metrics/basicmetrics/plots.py
+++ src/metrics/basicmetrics/plots.py
@@ -34,7 +34,7 @@ def get_training(path):
     # We read the csv into a pandas.Dataframe
     dfs = [pd.read_csv(path + file) for file in files]
     # We get the names of the heuristics.
-    names = [re.search("(training[A-Za-z0-9]*)\.csv$", file).group(1) for file in files]
+    names = [re.search("(training[_A-Za-z0-9]*)\.csv$", file).group(1) for file in files]
     for i in range(len(names)):
         dfs[i]["Heuristic"] = names[i]
 
@@ -43,37 +43,50 @@ def get_training(path):
     return training
 
 
-def node_total(eval, estimator=np.mean, ax=None):
+def save_fig(plot, save_path, name):
+    """
+    Save the plot at the location specified by `save_path` if `save_path` is not `None`.
+    """
+    if save_path is not None:
+        fig = plot.get_figure()
+        fig.savefig(save_path + name + ".png")
+
+
+def node_total(eval, estimator=np.mean, ax=None, save_path=None):
     plot = sns.lineplot(
         data=eval[eval["Solution"] == 0], y="Nodes", x="Episode", hue="Heuristic", estimator=estimator, ax=ax
     )
     plot.set(xlabel="Evaluation step", ylabel="Nodes visited", title="Node visited until optimality")
+    save_fig(plot, save_path, "eval_node_visited_optimality")
 
 
-def node_first(eval, estimator=np.mean, ax=None):
+def node_first(eval, estimator=np.mean, ax=None, save_path=None):
     first_solution = eval.loc[
         eval[eval["SolutionFound"] == 1].groupby(["Episode", "Instance", "Heuristic"])["Solution"].idxmin()
     ]
     plot = sns.lineplot(data=first_solution, y="Nodes", x="Episode", hue="Heuristic", estimator=estimator, ax=ax)
     plot.set(xlabel="Evaluation step", ylabel="Nodes visited", title="Node visited until first solution")
+    save_fig(plot, save_path, "eval_node_visited_first_solution")
 
 
-def score_first(eval, estimator=np.mean, ax=None):
+def score_first(eval, estimator=np.mean, ax=None, save_path=None):
     first_solution = eval.loc[
         eval[eval["SolutionFound"] == 1].groupby(["Episode", "Instance", "Heuristic"])["Solution"].idxmin()
     ]
     plot = sns.lineplot(data=first_solution, y="Score", x="Episode", hue="Heuristic", estimator=estimator, ax=ax)
     plot.set(xlabel="Evaluation step", ylabel="Score obtained", title="Score at first solution")
+    save_fig(plot, save_path, "eval_score_first_solution")
 
 
-def time_total(eval, estimator=np.mean, ax=None):
+def time_total(eval, estimator=np.mean, ax=None, save_path=None):
     plot = sns.lineplot(
         data=eval[eval["Solution"] == 0], y="Time", x="Episode", hue="Heuristic", estimator=estimator, ax=ax
     )
     plot.set(xlabel="Evaluation step", ylabel="Time needed", title="Time needed to prove optimatility")
+    save_fig(plot, save_path, "eval_time_optimality")
 
 
-def node_rollmean(training, window=100, ax=None):
+def node_rollmean(training, window=100, ax=None, save_path=None):
     df = (
         training[training["Solution"] == 0]
         .set_index("Episode")
@@ -91,9 +104,10 @@ def node_rollmean(training, window=100, ax=None):
             window
         ),
     )
+    save_fig(plot, save_path, "train_node_visited")
 
 
-def reward_rollmean(training, window=100, ax=None):
+def reward_rollmean(training, window=100, ax=None, save_path=None):
     df = (
         training[training["Solution"] == 0]
         .set_index("Episode")
@@ -109,9 +123,10 @@ def reward_rollmean(training, window=100, ax=None):
         ylabel="Reward obtained",
         title="Sliding average over the last {} episodes of the reward obtained.".format(window),
     )
+    save_fig(plot, save_path, "train_reward")
 
 
-def loss_rollmean(training, window=100, ax=None):
+def loss_rollmean(training, window=100, ax=None, save_path=None):
     df = (
         training[training["Solution"] == 0]
         .set_index("Episode")
@@ -127,11 +142,11 @@ def loss_rollmean(training, window=100, ax=None):
         ylabel="Loss",
         title="Sliding average over the last {} episodes of the loss.".format(window),
     )
+    save_fig(plot, save_path, "train_loss")
 
 
-def summary(eval, training, estimator=np.mean, window=100):
-    sns.set(rc={"figure.figsize": (24, 16)})
-    _, axs = plt.subplots(nrows=2, ncols=3)
+def summary(eval, training, estimator=np.mean, window=100, save_path=None):
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(24, 16), facecolor='white')
 
     node_total(eval, estimator=estimator, ax=axs[0][0])
     node_first(eval, estimator=estimator, ax=axs[0][1])
@@ -140,3 +155,34 @@ def summary(eval, training, estimator=np.mean, window=100):
     node_rollmean(training, window=window, ax=axs[1][0])
     reward_rollmean(training, window=window, ax=axs[1][1])
     loss_rollmean(training, window=window, ax=axs[1][2])
+    fig.savefig(save_path + "summary.png")
+
+
+def all(path, estimator=np.mean, window=100, save_path=None):
+    """
+    Saves all plots and displays them if run in a notebook. 
+
+    The files are saved in the same location as the data files, unless the `save_path` parameter is specified.
+    """
+    eval = get_eval(path)
+    training = get_training(path)
+
+    if save_path is None:
+        save_path = path
+
+    summary(eval, training, estimator=estimator, window=window, save_path=save_path)
+
+    sns.set(rc={"figure.figsize": (12, 8)})
+    _, ax = plt.subplots()
+    node_total(eval, estimator=estimator, save_path=save_path, ax=ax)
+    _, ax = plt.subplots()
+    node_first(eval, estimator=estimator, save_path=save_path, ax=ax)
+    if not eval["Score"].isnull().values.all():
+        _, ax = plt.subplots()
+        score_first(eval, estimator=estimator, save_path=save_path, ax=ax)
+    _, ax = plt.subplots()
+    node_rollmean(training, window=window, save_path=save_path)
+    _, ax = plt.subplots()
+    reward_rollmean(training, window=window, save_path=save_path)
+    _, ax = plt.subplots()
+    loss_rollmean(training, window=window, save_path=save_path)
