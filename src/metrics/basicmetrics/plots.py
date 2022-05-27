0472690e7784c6927d945d8deb018ef4e36eed80
Tristan François <tpv.francois@gmail.com>
2022-05-27 10:39:29 -0400

    Sort heuristic in order to keep the order across all plots

--- src/metrics/basicmetrics/plots.py
+++ src/metrics/basicmetrics/plots.py
@@ -7,6 +7,27 @@ import pandas as pd
 import seaborn as sns
 
 
+def key(name, heuristics):
+    if any(heuristic.endswith(name) for heuristic in heuristics):
+        return name
+    else:
+        return "zzz" + name
+
+
+def apply_key(series, training):
+    """
+    We add "zzz" at the beginning of the name of each heuristic 
+    that is not learned to put them at the end in the sort.
+
+    The objective is to keep the order of the heuristics between 
+    the plots related to the dataframe `training` and those related to the dataframe `eval`.
+    """
+    if training is None:
+        return series
+    heuristics = set(training["Heuristic"])
+    return series.apply(lambda x: key(x, heuristics))
+
+
 def get_eval(path):
     files = os.listdir(path)
     # We keep only csv file
@@ -52,35 +73,45 @@ def save_fig(plot, save_path, name):
         fig.savefig(save_path + name + ".png")
 
 
-def node_total(eval, estimator=np.mean, ax=None, save_path=None):
+def node_total(eval, estimator=np.mean, ax=None, save_path=None, training=None):
     plot = sns.lineplot(
-        data=eval[eval["Solution"] == 0], y="Nodes", x="Episode", hue="Heuristic", estimator=estimator, ax=ax
+        data=eval[eval["Solution"] == 0].sort_values("Heuristic", key=lambda series: apply_key(series, training)),
+        y="Nodes",
+        x="Episode",
+        hue="Heuristic",
+        estimator=estimator,
+        ax=ax,
     )
     plot.set(xlabel="Evaluation step", ylabel="Nodes visited", title="Node visited until optimality")
     save_fig(plot, save_path, "eval_node_visited_optimality")
 
 
-def node_first(eval, estimator=np.mean, ax=None, save_path=None):
+def node_first(eval, estimator=np.mean, ax=None, save_path=None, training=None):
     first_solution = eval.loc[
         eval[eval["SolutionFound"] == 1].groupby(["Episode", "Instance", "Heuristic"])["Solution"].idxmin()
-    ]
+    ].sort_values("Heuristic", key=lambda series: apply_key(series, training))
     plot = sns.lineplot(data=first_solution, y="Nodes", x="Episode", hue="Heuristic", estimator=estimator, ax=ax)
     plot.set(xlabel="Evaluation step", ylabel="Nodes visited", title="Node visited until first solution")
     save_fig(plot, save_path, "eval_node_visited_first_solution")
 
 
-def score_first(eval, estimator=np.mean, ax=None, save_path=None):
+def score_first(eval, estimator=np.mean, ax=None, save_path=None, training=None):
     first_solution = eval.loc[
         eval[eval["SolutionFound"] == 1].groupby(["Episode", "Instance", "Heuristic"])["Solution"].idxmin()
-    ]
+    ].sort_values("Heuristic", key=lambda series: apply_key(series, training))
     plot = sns.lineplot(data=first_solution, y="Score", x="Episode", hue="Heuristic", estimator=estimator, ax=ax)
     plot.set(xlabel="Evaluation step", ylabel="Score obtained", title="Score at first solution")
     save_fig(plot, save_path, "eval_score_first_solution")
 
 
-def time_total(eval, estimator=np.mean, ax=None, save_path=None):
+def time_total(eval, estimator=np.mean, ax=None, save_path=None, training=None):
     plot = sns.lineplot(
-        data=eval[eval["Solution"] == 0], y="Time", x="Episode", hue="Heuristic", estimator=estimator, ax=ax
+        data=eval[eval["Solution"] == 0].sort_values("Heuristic", key=lambda series: apply_key(series, training)),
+        y="Time",
+        x="Episode",
+        hue="Heuristic",
+        estimator=estimator,
+        ax=ax,
     )
     plot.set(xlabel="Evaluation step", ylabel="Time needed", title="Time needed to prove optimatility")
     save_fig(plot, save_path, "eval_time_optimality")
@@ -146,7 +177,7 @@ def loss_rollmean(training, window=100, ax=None, save_path=None):
 
 
 def summary(eval, training, estimator=np.mean, window=100, save_path=None):
-    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(24, 16), facecolor='white')
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(24, 16), facecolor="white")
 
     node_total(eval, estimator=estimator, ax=axs[0][0])
     node_first(eval, estimator=estimator, ax=axs[0][1])
@@ -160,7 +191,7 @@ def summary(eval, training, estimator=np.mean, window=100, save_path=None):
 
 def all(path, estimator=np.mean, window=100, save_path=None):
     """
-    Saves all plots and displays them if run in a notebook. 
+    Saves all plots and displays them if run in a notebook.
 
     The files are saved in the same location as the data files, unless the `save_path` parameter is specified.
     """
@@ -174,12 +205,12 @@ def all(path, estimator=np.mean, window=100, save_path=None):
 
     sns.set(rc={"figure.figsize": (12, 8)})
     _, ax = plt.subplots()
-    node_total(eval, estimator=estimator, save_path=save_path, ax=ax)
+    node_total(eval, estimator=estimator, save_path=save_path, ax=ax, training=training)
     _, ax = plt.subplots()
-    node_first(eval, estimator=estimator, save_path=save_path, ax=ax)
+    node_first(eval, estimator=estimator, save_path=save_path, ax=ax, training=training)
     if not eval["Score"].isnull().values.all():
         _, ax = plt.subplots()
-        score_first(eval, estimator=estimator, save_path=save_path, ax=ax)
+        score_first(eval, estimator=estimator, save_path=save_path, ax=ax, training=training)
     _, ax = plt.subplots()
     node_rollmean(training, window=window, save_path=save_path)
     _, ax = plt.subplots()
